9ac975958dd1da8a9d96ed944f8d8355ccc6a0b0
NJVS <neiljonathanv.sagisi+github@gmail.com>
2022-12-23 15:32:27 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+
+function App() {
+  return (
+    <>
+      <header></header>
+      <main></main>
+      <footer>
+        <p className="attribution">
+          Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+          Coded by <a href="#">lieneil</a>.
+        </p>
+      </footer>
+    </>
+  );
+}
+
+export default App;
